1158d48a2b597f963441cf9f5efe368fe9a5a29f
utopictown <utopictown@meerve.com>
2021-05-01 02:35:52 +0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,38 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Meerve - Let music connects you</title>
+        <meta name="description" content="Meerve - let music connects you" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Jua&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic+Coding&display=swap" rel="stylesheet"/>
+      </Head>
+
+      <main className="typewriter">
+        <h1>Meerve</h1>
+        <h3>Let music connects you</h3>
+        <a href="https://twitter.com/meerveapp" target="_blank">@meerveapp</a>
+      </main>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+        <div class="firefly"></div>
+    </div>
+  );
+}
