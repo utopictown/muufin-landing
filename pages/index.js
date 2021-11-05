677492a7f3d576d2667712cbe2d0562b7efeaa99
utopictown <42117448+utopictown@users.noreply.github.com>
2021-11-05 15:45:45 +0700

    refactor: :adhesive_bandage: update app name

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Meerve - Let music connects you</title>
-        <meta name="description" content="Meerve - let music connects you" />
+        <title>Merrve - Let music connects you</title>
+        <meta name="description" content="Merrve - let music connects you" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Jua&display=swap" rel="stylesheet"/>
@@ -14,9 +14,9 @@ export default function Home() {
       </Head>
 
       <main className="typewriter">
-        <h1>Meerve</h1>
+        <h1>Merrve</h1>
         <h3>Let music connects you</h3>
-        <a href="https://twitter.com/meerveapp" target="_blank">@meerveapp</a>
+        <a href="https://twitter.com/merrveapp" target="_blank">@merrveapp</a>
       </main>
         <div class="firefly"></div>
         <div class="firefly"></div>
