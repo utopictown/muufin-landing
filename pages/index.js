db26bf0283b7ca1b56c8ad83e739bd405ef3a8b6
utopictown <42117448+utopictown@users.noreply.github.com>
2021-11-07 00:27:01 +0700

    style: :adhesive_bandage: patch app name meta

--- pages/index.js
+++ pages/index.js
@@ -118,11 +118,11 @@ export default function Home() {
           rel="stylesheet"
         />
 
-        <meta name="application-name" content="PWA App" />
+        <meta name="application-name" content="Merrve" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="PWA App" />
-        <meta name="description" content="Best PWA App in the world" />
+        <meta name="apple-mobile-web-app-title" content="Merrve" />
+        <meta name="description" content="Let music connects you" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="msapplication-config" content="/icons/browserconfig.xml" />
