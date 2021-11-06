a3759a69c4eab34271a65400dfcfe11bbca44ee5
utopictown <42117448+utopictown@users.noreply.github.com>
2021-11-07 01:09:07 +0700

    fix: :fire: remove manifest

--- pages/index.js
+++ pages/index.js
@@ -108,7 +108,6 @@ export default function Home() {
           sizes="16x16"
           href="/favicon-16x16.png"
         />
-        <link rel="manifest" href="/site.webmanifest" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Jua&display=swap"
