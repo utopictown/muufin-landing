ac4e8a9b97c07c913f30481ab778e8b9ccecbc51
utopictown <42117448+utopictown@users.noreply.github.com>
2021-11-07 00:46:56 +0700

    fix: :bug: import usestate

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
