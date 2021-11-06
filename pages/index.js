7ea4d0566ec30f2a87415a54a0931e63c0283e7b
utopictown <42117448+utopictown@users.noreply.github.com>
2021-11-07 00:24:55 +0700

    style: :lipstick: add meta

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,94 @@
 import Head from "next/head";
+import { useEffect } from "react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  useEffect(() => {
+    if (
+      typeof window !== "undefined" &&
+      "serviceWorker" in navigator &&
+      window.workbox !== undefined
+    ) {
+      const wb = window.workbox;
+      // add event listeners to handle any of PWA lifecycle event
+      // https://developers.google.com/web/tools/workbox/reference-docs/latest/module-workbox-window.Workbox#events
+      wb.addEventListener("installed", (event) => {
+        console.log(`Event ${event.type} is triggered.`);
+        console.log(event);
+      });
+
+      wb.addEventListener("controlling", (event) => {
+        console.log(`Event ${event.type} is triggered.`);
+        console.log(event);
+      });
+
+      wb.addEventListener("activated", (event) => {
+        console.log(`Event ${event.type} is triggered.`);
+        console.log(event);
+      });
+
+      // A common UX pattern for progressive web apps is to show a banner when a service worker has updated and waiting to install.
+      // NOTE: MUST set skipWaiting to false in next.config.js pwa object
+      // https://developers.google.com/web/tools/workbox/guides/advanced-recipes#offer_a_page_reload_for_users
+      const promptNewVersionAvailable = (event) => {
+        // `event.wasWaitingBeforeRegister` will be false if this is the first time the updated service worker is waiting.
+        // When `event.wasWaitingBeforeRegister` is true, a previously updated service worker is still waiting.
+        // You may want to customize the UI prompt accordingly.
+        if (
+          confirm(
+            "A newer version of this web app is available, reload to update?"
+          )
+        ) {
+          wb.addEventListener("controlling", (event) => {
+            window.location.reload();
+          });
+
+          // Send a message to the waiting service worker, instructing it to activate.
+          wb.messageSkipWaiting();
+        } else {
+          console.log(
+            "User rejected to reload the web app, keep using old version. New version will be automatically load when user open the app next time."
+          );
+        }
+      };
+
+      wb.addEventListener("waiting", promptNewVersionAvailable);
+
+      // ISSUE - this is not working as expected, why?
+      // I could only make message event listenser work when I manually add this listenser into sw.js file
+      wb.addEventListener("message", (event) => {
+        console.log(`Event ${event.type} is triggered.`);
+        console.log(event);
+      });
+
+      /*
+      wb.addEventListener('redundant', event => {
+        console.log(`Event ${event.type} is triggered.`)
+        console.log(event)
+      })
+      wb.addEventListener('externalinstalled', event => {
+        console.log(`Event ${event.type} is triggered.`)
+        console.log(event)
+      })
+      wb.addEventListener('externalactivated', event => {
+        console.log(`Event ${event.type} is triggered.`)
+        console.log(event)
+      })
+      */
+
+      // never forget to call register as auto register is turned off in next.config.js
+      wb.register();
+    }
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Merrve - Let music connects you</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
         <meta name="description" content="Merrve - let music connects you" />
         <link
           rel="apple-touch-icon"
@@ -34,6 +117,37 @@ export default function Home() {
           href="https://fonts.googleapis.com/css2?family=Nanum+Gothic+Coding&display=swap"
           rel="stylesheet"
         />
+
+        <meta name="application-name" content="PWA App" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="PWA App" />
+        <meta name="description" content="Best PWA App in the world" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+        />
+
+        <meta name="twitter:card" content="Merrve - Let music connects you" />
+        <meta name="twitter:url" content="https://merrve.com" />
+        <meta name="twitter:title" content="Merrve" />
+        <meta name="twitter:description" content="Let music connects you" />
+        <meta name="twitter:image" content="/android-chrome-192x192.png" />
+        <meta name="twitter:creator" content="@utopictown" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Merrve" />
+        <meta property="og:description" content="Let music connects you" />
+        <meta property="og:site_name" content="Merrve" />
+        <meta property="og:url" content="https://merrve.com" />
+        <meta property="og:image" content="/apple-touch-icon.png" />
       </Head>
 
       <main className="typewriter">
