3440d8ec7ce984e0ab1d6c9507af6dd9b16fd948
utopictown <42117448+utopictown@users.noreply.github.com>
2021-11-06 23:24:22 +0700

    fix: :adhesive_bandage: add noreferrer

--- pages/index.js
+++ pages/index.js
@@ -7,35 +7,61 @@ export default function Home() {
       <Head>
         <title>Merrve - Let music connects you</title>
         <meta name="description" content="Merrve - let music connects you" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/site.webmanifest"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=Jua&display=swap" rel="stylesheet"/>
-        <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic+Coding&display=swap" rel="stylesheet"/>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Jua&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nanum+Gothic+Coding&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <main className="typewriter">
         <h1>Merrve</h1>
         <h3>Let music connects you</h3>
-        <a href="https://twitter.com/merrveapp" target="_blank">@merrveapp</a>
+        <a
+          href="https://twitter.com/merrveapp"
+          target="_blank"
+          rel="noreferrer"
+        >
+          @merrveapp
+        </a>
       </main>
-        <div className="firefly"></div>
-        <div className="firefly"></div>
-        <div className="firefly"></div>
-        <div className="firefly"></div>
-        <div className="firefly"></div>
-        <div className="firefly"></div>
-        <div className="firefly"></div>
-        <div className="firefly"></div>
-        <div className="firefly"></div>
-        <div className="firefly"></div>
-        <div className="firefly"></div>
-        <div className="firefly"></div>
-        <div className="firefly"></div>
-        <div className="firefly"></div>
-        <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
+      <div className="firefly"></div>
     </div>
   );
 }
